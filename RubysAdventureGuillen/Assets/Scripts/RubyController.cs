0435cd770a7dedb092cfdeec667d456ce2d84938
710619EG <116373777+710619EG@users.noreply.github.com>
2022-12-01 13:18:41 -0800

    did a lot of stuff will describe in next push origin after this

--- RubysAdventureGuillen/Assets/Scripts/RubyController.cs
+++ RubysAdventureGuillen/Assets/Scripts/RubyController.cs
@@ -7,14 +7,25 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //This will make unity run the game at 10 frames per second if it were made as actual code
+        //QualitySettings.vSyncCount = 0;Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //create 2 variables to use the unity built in axes
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        //Create our movement vector
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+
+        //Create Horizantal and vertical movement
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        //setting the new position
         transform.position = position;
     }
 }
